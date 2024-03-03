34066bfed5c7d0bddc06f741421d32dbb3289ef1
satoki252595 <satoki252595@gmail.com>
2024-03-03 10:01:01 +0900

    CLI作成中

--- operation.sh
+++ operation.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-echo "現新比較ツールの設定を始めます。"
-
 ##設定ファイルを編集する。
 is_setting() {
-    read -p "設定が必要な場合はyを押下して下さい。y/n：" yn_flag
+    echo "初期設定を行います。初めて利用する方はyを押して下さい。"
+    read -p "y/n：" yn_flag
     if [ $yn_flag = "y" ]
     then
         echo "設定を始めます。"
-        ##設定関数を呼ぶ。今はコールできていない。
+        ##設定関数を呼ぶ。今は関数を作っていない。
         return 0
     elif [ $yn_flag = "n" ]
     then
-        echo "設定は行いません。"
+
         return 0
     else
-        echo "設定をする場合はy、設定をしない場合はnを押下して下さい。"
+        echo "初期設定をする場合はy、初期設定をしない場合はnを押下して下さい。"
         return 1
     fi
 }
+
+##ここからmain処理となる。
+
+echo "現新比較ツールの設定を始めます。"
 
 while :
 do
